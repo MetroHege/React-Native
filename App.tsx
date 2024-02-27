2e06434a8670ab45231b0f599a576c9247a77662
MetroHege <113589548+MetroHege@users.noreply.github.com>
2024-02-27 14:04:32 +0200

    .

--- App.tsx
+++ App.tsx
@@ -2,22 +2,22 @@ import {StatusBar} from 'expo-status-bar';
 import {Platform, SafeAreaView, StyleSheet, Text} from 'react-native';
 
 const App = () => {
+  console.log('moro');
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello, React Native!</Text>
+      <Text>HeLLO!</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
 };
 
+export default App;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: Platform.OS === 'android' ? 30 : 0,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? 30 : 0,
   },
 });
-
-export default App;
